d63dcea177fe233e9ecbb60a52f03b4ac9f4f687
Scroker <dramisgiorgio@outlook.com>
2022-05-25 01:35:55 +0200

    Initial Commit

---
+++ src/main/java/org/acme/resource/GreetingResource.java
@@ -0,0 +1,41 @@
+package org.acme;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    GreetingService service;
+
+    @ConfigProperty(name = "greeting.message") 
+    String message;
+    
+    @ConfigProperty(name = "greeting.suffix", defaultValue="!") 
+    String suffix;
+    
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name; 
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("{name}")
+    public String greeting(@PathParam("name") String name) {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return message + " " + name.orElse("world") + suffix;
+    }
+}
---
+++ src/main/java/org/acme/service/SantaClausService.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.acme.model.Gift;
+
+@ApplicationScoped
+public class SantaClausService {
+    
+    @Inject
+    EntityManager em; 
+
+    @Transactional 
+    public void createGift(String giftDescription) {
+        Gift gift = new Gift();
+        gift.setName(giftDescription);
+        em.persist(gift);
+    }
+}
